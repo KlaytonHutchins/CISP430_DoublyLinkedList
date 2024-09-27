e99c30c51cb6bd63ec5be8086b45d5d72fc26f05
Klayton Hutchins <klaytonhutchins@gmail.com>
2024-09-26 19:13:02 -0700

    create cpp file, stub out classes and functions

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//Change file names here if applicable
+string inFileName = "input.txt";
+string outFileName = "output.txt"
+
+class Node {
+public:
+private:
+};
+
+class DoublyLL {
+public:
+private:
+};
+
+//Helper function to convert text to all lower case
+string toLower(string str) {
+}
+
+int main() {
+}
