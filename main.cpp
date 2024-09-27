d25312ed561b00d760ca7abe6e11231d0bdb13d9
Klayton Hutchins <klaytonhutchins@gmail.com>
2024-09-26 19:17:01 -0700

    finish node class

--- main.cpp
+++ main.cpp
@@ -7,9 +7,39 @@ using namespace std;
 string inFileName = "input.txt";
 string outFileName = "output.txt"
 
+//Node class with previous and next nodes.
+//Member variables are private, must use getter and setter functions.
 class Node {
 public:
+        //Construct new node with a name
+        Node(string initName) {
+                name = initName;
+                next = nullptr;
+                prev = nullptr;
+        }
+        //Getters and Setters
+        void setName(string n) {
+                name = n;
+        }
+        string getName() {
+                return name;
+        }
+        void setNext(Node* n) {
+                next = n;
+        }
+        Node* getNext() {
+                return next;
+        }
+        void setPrev(Node* p) {
+                prev = p;
+        }
+        Node* getPrev() {     
+                return prev;
+        }
 private:
+        string name;
+        Node* next;
+        Node* prev;
 };
 
 class DoublyLL {
