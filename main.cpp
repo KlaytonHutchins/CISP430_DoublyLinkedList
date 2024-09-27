14b8d2f53cdc152e636aa630be953a1ddbd07400
Klayton Hutchins <klaytonhutchins@gmail.com>
2024-09-26 19:21:19 -0700

    finish doubly linked list class

--- main.cpp
+++ main.cpp
@@ -44,7 +44,103 @@ private:
 
 class DoublyLL {
 public:
+        //Construct new empty Doubly Linked List
+        DoublyLL() {
+                head = nullptr;
+                tail = nullptr;
+        }
+        //Destructor
+        ~DoublyLL() {
+                Node* curr = head;
+                while (curr) {
+                        Node* tempNext = curr->getNext();
+                        delete curr;
+                        curr = tempNext;
+                }
+        }
+        //Insert function
+        void insert(string valueToInsert) {
+                Node* newNode = new Node(valueToInsert);
+                if (!head) {
+                        head = newNode;
+                        tail = newNode;
+                } else if (valueToInsert < head->getName()) {
+                        newNode->setNext(head);
+                        head->setPrev(newNode);
+                        head = newNode;
+                } else if (valueToInsert > tail->getName()) {
+                        newNode->setPrev(tail);
+                        tail->setNext(newNode);
+                        tail = newNode;
+                } else {
+                        Node* curr = head;
+                        while(curr) {
+                                if (valueToInsert < curr->getName()) {
+                                        newNode->setNext(curr);
+                                        newNode->setPrev(curr->getPrev());
+                                        curr->getPrev()->setNext(newNode);
+                                        curr->setPrev(newNode);
+                                        break;
+                                }
+                                curr = curr->getNext();
+                        }
+                }
+        }
+        //Delete function
+        void deleteNode(string nameToDelete) {
+                Node* curr = head;
+                while (curr) {
+                        if (curr->getName() == nameToDelete) {
+                                if (curr->getPrev()) {
+                                        curr->getPrev()->setNext(curr->getNext());
+                                } else {
+                                        head = curr->getNext();
+                                }
+                                if (curr->getNext()) {
+                                        curr->getNext()->setPrev(curr->getPrev());
+                                } else {
+                                        tail = curr->getPrev();
+                                }
+                                delete curr;
+                        } else {
+                                curr = curr->getNext();
+                        }
+                }
+        }
+        //Traverse function, uses ofstream to pass data back to main
+        ofstream& traverseAscending(ofstream& outFile) {
+                Node* curr = head;
+                while (curr) {
+                        outFile << curr->getName() << endl;
+                        curr = curr->getNext();
+                }
+                return outFile;
+        }
+        //Traverse function, uses ofstream to pass data back to main
+        ofstream& traverseDescending(ofstream& outFile) {
+                Node* curr = tail;
+                while (curr) {
+                        outFile << curr->getName() << endl;
+                        curr = curr->getPrev();
+                }
+                return outFile;
+        }
+        //Getters and Setters   
+        void setHead(Node* h) {
+                head = h;
+        }
+        Node* getHead() {
+                return head;
+        }
+        void setTail(Node* t) {
+                tail = t;
+        }
+        Node* getTail() {
+                return tail;
+        }
 private:
+        Node* head;
+        Node* tail;
 };
 
 //Helper function to convert text to all lower case
